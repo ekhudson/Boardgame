554f6f8c86df966c62a0aca773487496515f2ce7
ekhudson <ekhudson@gmail.com>
2015-12-15 22:45:32 -0800

    First commit!

---
+++ Assets/SliderTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderTest : MonoBehaviour {
+
+
+    private UnityEngine.UI.Slider mSlider;
+
+    private void Start()
+    {
+        mSlider = GetComponent<UnityEngine.UI.Slider>();
+    }
+
+    public void Increment(int amount)
+    {
+        if (mSlider != null)
+        {
+            mSlider.value += amount;
+        }
+    }
+
+}
